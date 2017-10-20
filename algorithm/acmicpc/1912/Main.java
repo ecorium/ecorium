7b4a91ed3daeca4710782d038b9a86e916e4cf22
Kitae Kim <kt920.kim@samsung.com>
2017-10-20 17:28:09 +0900

    Update solution for 1912 problem.
    Signed-off-by: Kitae Kim <kt920.kim@samsung.com>

--- algorithm/acmicpc/1912/Main.java
+++ algorithm/acmicpc/1912/Main.java
@@ -6,42 +6,31 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		
-		System.setIn(new FileInputStream("sample.txt"));
+//		System.setIn(new FileInputStream("sample.txt"));
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		
 		int N = Integer.parseInt(br.readLine().trim());
-		int[] dp = new int[N];
-		int[] arr = new int[N];
+		int[] dp = new int[N+1];
+		int[] arr = new int[N+1];
 		
 		String[] values = br.readLine().trim().split(" ");
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(values[i]);
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(values[i-1]);
 		}
 
 		int answer = Integer.MIN_VALUE;
-		dp[0] = arr[0];
-		answer = Math.max(answer, dp[0]);
 		
-		for (int i = 0; i < N-1; i++) {
-			if (dp[i] + arr[i+1] > 0) {
+		for (int i = 0; i <= N-1; i++) {
+			if (dp[i] + arr[i+1] > arr[i+1]) {
 				dp[i+1] = dp[i] + arr[i+1];
 			} else {
-				dp[i+1] = 0;
+				dp[i+1] = arr[i+1];
 			}
 			answer = Math.max(answer, dp[i+1]);
 		}
 		
-		for (int i = 0; i < N; i++) {
-			System.out.print(dp[i] + " ");
-		}
-		System.out.println();
-		
 		System.out.println(answer);
 		br.close();
-
 	}
-
 }
